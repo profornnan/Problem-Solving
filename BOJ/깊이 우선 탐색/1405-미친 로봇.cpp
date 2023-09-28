144e03eab0dbd10bd9102cee902d78f6112ea515
profornnan <profornnan@naver.com>
2023-09-28 23:34:27 +0900

    [UPDATE] BOJ-1405-미친 로봇.cpp

--- BOJ/깊이 우선 탐색/1405-미친 로봇.cpp
+++ BOJ/깊이 우선 탐색/1405-미친 로봇.cpp
@@ -1,43 +1,51 @@
 #include <iostream>
-#define sws ios::sync_with_stdio(false), cin.tie(NULL)
 using namespace std;
 
-const int OFFSET = 15;
+constexpr int OFFSET = 14;
 
-int dx[] = { 1, -1, 0, 0 };  // E, W, S, N
-int dy[] = { 0, 0, 1, -1 };
-int n;
+constexpr int dx[]{ 1, -1, 0, 0 };
+constexpr int dy[]{ 0, 0, 1, -1 };
+
+int N;
 int percent[4];
-bool visited[31][31];
+bool visited[OFFSET * 2 + 1][OFFSET * 2 + 1];
 double ans;
 
 void dfs(int cnt, int y, int x, double per) {
-	if (cnt == n) {
+	if (cnt >= N) {
 		ans += per;
 		return;
 	}
-	for (int i = 0; i < 4; i++) {
-		int ny = y + dy[i];
-		int nx = x + dx[i];
-		if (!visited[ny][nx]) {
-			visited[ny][nx] = true;
-			dfs(cnt + 1, ny, nx, per * percent[i] / 100);
-			visited[ny][nx] = false;
-		}
+
+	for (int dir = 0; dir < 4; ++dir) {
+		int ny = y + dy[dir];
+		int nx = x + dx[dir];
+
+		if (visited[ny][nx])
+			continue;
+
+		visited[ny][nx] = true;
+		dfs(cnt + 1, ny, nx, per * percent[dir] / 100);
+		visited[ny][nx] = false;
 	}
 }
 
-int main(void)
-{
-	sws;
-	freopen("input.txt", "r", stdin);
+int main(void) {
+	cin.tie(nullptr)->sync_with_stdio(false);
+
 	cout << fixed;
 	cout.precision(10);
-	cin >> n;
-	for (int i = 0; i < 4; i++)
-		cin >> percent[i];
+
+	cin >> N;
+
+	for (int dir = 0; dir < 4; ++dir)
+		cin >> percent[dir];
+
 	visited[OFFSET][OFFSET] = true;
 	dfs(0, OFFSET, OFFSET, 1);
+	visited[OFFSET][OFFSET] = false;
+
 	cout << ans << '\n';
+
 	return 0;
 }
