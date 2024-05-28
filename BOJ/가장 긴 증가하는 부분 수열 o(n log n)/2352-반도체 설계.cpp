846fd537d2df1e14c5eee1d593891f9071f137d1
profornnan <profornnan@naver.com>
2024-05-28 12:14:14 +0900

    [ADD] BOJ-2352-반도체 설계.cpp

---
+++ BOJ/가장 긴 증가하는 부분 수열 o(n log n)/2352-반도체 설계.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int lowerBound(const vector<int>& arr, const int target) {
+	int left = 0;
+	int right = arr.size() - 1;
+
+	while (left < right) {
+		int mid = left + (right - left) / 2;
+
+		if (arr[mid] < target)
+			left = mid + 1;
+		else
+			right = mid;
+	}
+
+	return right;
+}
+
+int main(void) {
+	cin.tie(nullptr)->sync_with_stdio(false);
+
+	int N;
+	cin >> N;
+
+	vector<int> arr(N);
+	vector<int> seq;
+
+	for (int i = 0; i < N; ++i)
+		cin >> arr[i];
+
+	seq.push_back(arr[0]);
+
+	for (int i = 1; i < N; ++i) {
+		if (seq.back() < arr[i])
+			seq.emplace_back(arr[i]);
+		else
+			seq[lowerBound(seq, arr[i])] = arr[i];
+	}
+
+	cout << seq.size() << '\n';
+
+	return 0;
+}
